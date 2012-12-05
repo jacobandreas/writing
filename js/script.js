a0a041c1be6a49999747a91e408db40360922d0a
Jacob <andqso@gmail.com>
2012-12-05 12:42:13 +0000

    collapsible churchill letters

---
+++ js/script.js
@@ -0,0 +1,35 @@
+function setup() {
+  $('.collapse').each(function(idx) {
+    collapser = $(this);
+    toggler = collapser.prev();
+    toggler.wrapInner('<a href="#">');
+    toggle_marker = $('<span class="togglemarker"></span>');
+    toggle_marker.appendTo(toggler);
+    toggler.click(function() {
+      collapser.toggle();
+      if (toggle_marker.hasClass('closed')) {
+        toggle_marker.html(' &blacktriangledown;');
+        toggle_marker.removeClass('closed');
+        localStorage['closed'] = 'false';
+      } else {
+        toggle_marker.html(' &blacktriangleright;');
+        toggle_marker.addClass('closed');
+        localStorage['closed'] = 'true';
+      }
+    });
+    if (localStorage['closed'] === null) {
+      localStorage['closed'] = 'false';
+    }
+    if (localStorage['closed'] === 'true') {
+      toggle_marker.html(' &blacktriangleright;');
+      toggle_marker.addClass('closed');
+      collapser.hide();
+    } else {
+      toggle_marker.html(' &blacktriangledown;');
+    }
+  });
+}
+
+$(document).ready(function() {
+  setup();
+});
